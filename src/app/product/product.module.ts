6c892bcca2c4be1d37b9e4b0b0f304717ec9e5b7
aimbottsqll <gihressza12@hotmail.com>
2022-12-19 16:32:28 +0700

    first commit

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from "@angular/common/http";
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService} from 'primeng/api';
+
+
+
+import { ProductComponent } from './product.component';
+import { AddEditProductModule } from './add-edit-product/add-edit-product.module';
+import { MessageService } from 'primeng/api';
+
+@NgModule({
+  declarations: [
+    ProductComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    TableModule,
+    ButtonModule,
+    AddEditProductModule,
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  exports: [
+    ProductComponent
+  ],
+  providers: [MessageService, ConfirmationService]
+})
+export class ProductModule { }
---
+++ src/app/product/product.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>('https://fakestoreapi.com/products?sort=desc');
+  }
+
+  addEditProduct(postData: any, selectedPdt:any) {
+    if(!selectedPdt){
+      return this.http.post('https://fakestoreapi.com/products', postData);
+    } else {
+      return this.http.put(`https://fakestoreapi.com/products/${selectedPdt.id}`, postData);
+    }
+  }
+
+  deleteProduct(productId: number){
+    return this.http.delete(`https://fakestoreapi.com/products/${productId}`);
+  }
+}
